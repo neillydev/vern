0cbb778fe55ad6ee23027e87e851cf1161b51c3d
neillycubed <neillybusiness3@gmail.com>
2023-09-11 01:59:47 -0700

    add styling and JSX to Hero component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import Module from '../Module/Module';
 
 import styles from './Hero.module.css';
 
 const Hero = () => {
   return (
     <section className={`${styles.heroContainer} relative`}>
-      
+      <div className={`${styles.heroWrapper} relative text-center`}>
+        <div className={`${styles.widget}`}>
+          <div className={`${styles.widgetText}`}><span>Blog</span> ✦ Introducing VERN. AI</div>
+        </div>
+        <div className={`${styles.heroHeader} text-center`}>
+          <h1>AI For <span className={`${styles.headerModifier}`}>People</span></h1>
+          <p className={`${styles.subheader} text-center`}>Bridging the Gap, Unlocking Potential</p>
+        </div>
+        <Module />
+      </div>
     </section>
   )
 }
