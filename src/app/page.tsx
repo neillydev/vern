0d80cae87926703d8402a16451e37db039dc2a58
neillycubed <neillybusiness3@gmail.com>
2023-09-11 16:42:30 -0700

    refactor styling of hero components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center">
       <Navigation />
       <Hero />
     </main>
