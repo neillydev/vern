78453434142aff90ee9c1266387bb5c2cda4d6cd
neillycubed <neillybusiness3@gmail.com>
2023-09-11 16:50:48 -0700

    refactor styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
+    <main className="flex min-h-screen flex-col items-center overflow-x-hidden">
       <Navigation />
       <Hero />
     </main>
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,7 +6,7 @@ import styles from './Hero.module.css';
 
 const Hero = () => {
   return (
-    <section className={`${styles.heroContainer} relative`}>
+    <section className={`${styles.heroContainer} relative overflow-hidden`}>
       {/* <div className={`${styles.heroGrid}`}>
         <Image src='https://surfwaves.b-cdn.net/neillydev/grid_7.png' alt='' width={1024} height={1024} />
       </div> */}
