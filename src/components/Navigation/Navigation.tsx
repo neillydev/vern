9f11010b3acf79b808b7d8557c118ac23841dbb6
neillycubed <neillybusiness3@gmail.com>
2023-09-12 01:42:53 -0700

    add links to home page

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -10,11 +10,11 @@ const Navigation = () => {
   return (
     <section className={`${styles.navHeader}`}>
       <div className={`${styles.navWrapper} flex justify-between items-center`}>
-        <div className="flex items-center">
+        <a href="/" className="flex items-center">
           {/* <VernSVG className={`${styles.logo}`} /> */}
           <Image src='https://surfwaves.b-cdn.net/neillydev/vern_rounded.png' alt='' width={36} height={36} />
-          <h1 className={`${styles.title}`}>VERN.</h1>
-        </div>
+          <div className={`${styles.title}`}>VERN.</div>
+        </a>
         <ul className={`${styles.navCenter}`}>
           <li>
             <a href="" className={`${styles.navCenterControl}`}>Product</a>
@@ -30,8 +30,8 @@ const Navigation = () => {
           </li>
         </ul>
         <div className={`${styles.navControls} flex items-center`}>
-          <a href="" className={`${styles.navBtn} mr-8`}>Login</a>
-          <a href="" className={`${styles.navModule} ${styles.navBtn}`}>
+          <a href="/auth" className={`${styles.navBtn} mr-8`}>Login</a>
+          <a href="/auth" className={`${styles.navModule} ${styles.navBtn}`}>
             Sign Up
             <div className={`${styles.navModuleBorder}`} />
           </a>
