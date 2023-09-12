c2ac48dc36027ee3f0b4e694a0fe95b072d38d20
neillycubed <neillybusiness3@gmail.com>
2023-09-11 18:03:53 -0700

    create Auth page

---
+++ src/app/auth/page.js
@@ -0,0 +1,9 @@
+import styles from './Auth.module.css';
+
+export default function Auth() {
+  return (
+    <div className={`${styles.authContainer} fixed inset-0 text-white h-full overflow-y-auto`}>
+
+    </div>
+  )
+}
