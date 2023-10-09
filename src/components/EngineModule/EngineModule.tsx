7d3c1b81b1c7f05a3417a6029d701136a3660775
neillycubed <neillybusiness3@gmail.com>
2023-10-08 17:34:07 -0700

    add state handling and styling to engine module

--- src/components/EngineModule/EngineModule.tsx
+++ src/components/EngineModule/EngineModule.tsx
@@ -1,6 +1,9 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
+import { EngineState } from '../EngineDash/EngineDash';
+import Spinner from '../Spinner/Spinner';
+import Dropdown from '../Dropdown/Dropdown';
 
 import EngineSVG from '@/../public/icons/engine.svg';
 
@@ -10,19 +13,25 @@ type EngineModuleProps = {
   name: string;
   subheader: string;
   type: string;
+  state: EngineState;
 };
 
-const EngineModule = ({ name, subheader, type }: EngineModuleProps) => {
+const EngineModule = ({ name, subheader, type, state }: EngineModuleProps) => {
     const [loading, setLoading] = useState(false);
+    const [isDropdownOpen, setDropdownOpen] = useState(false);
+    
   return (
-    <div className={`${styles.eModContainer}`}>
+    <div className={`${styles.eModContainer}`} onClick={() => setDropdownOpen(true)}>
         <div className={`${styles.eModWrapper}`}>
           { type.toLowerCase() === 'core' ? <EngineSVG /> : <></> }
         </div>
         <div className={`${styles.eModHeader}`}>
           <h1>{name}</h1>
           <h3>{subheader}</h3>
+          <h3 className={`${state === EngineState.ACTIVE ? styles.stateActive : (state === EngineState.INACTIVE ? styles.stateInactive : styles.stateNR)}`}>{state}</h3>
         </div>
+                          
+        {isDropdownOpen && <Dropdown setDropdownOpen={setDropdownOpen} />}
     </div>
   )
 }
